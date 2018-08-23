459b9a7a78bd3a27906eed8a6e99fa5489809f87
Adam Schubert <adam.schubert@sg1-game.net>
2018-08-23 05:19:05 +0200

    Added more info into setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
+import sys
 from setuptools import setup
+
+if sys.version_info >= (3, 0):
+    long_description = open('README.md', encoding='utf-8').read()
+else:
+    long_description = open('README.md').read()
 
 setup(
     name='huawei-lte-api',
@@ -19,7 +25,29 @@ setup(
     author='Adam Schubert',
     author_email='adam.schubert@sg1-game.net',
     description='API For huawei LAN/WAN LTE Modems',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     test_suite="tests",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.5",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development",
+    ],
     setup_requires=[
         'pytest-runner'
     ],
