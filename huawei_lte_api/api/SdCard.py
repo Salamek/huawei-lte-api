7a1c764f242cc1bf7ba1c5d8dd3f2bea817c8d77
Ville Skyttä <ville.skytta@iki.fi>
2018-10-18 23:15:04 +0300

    Add more sdcard and webserver methods

--- huawei_lte_api/api/SdCard.py
+++ huawei_lte_api/api/SdCard.py
@@ -1,4 +1,5 @@
 from huawei_lte_api.ApiGroup import ApiGroup
+from huawei_lte_api.AuthorizedConnection import authorized_call
 
 
 class SdCard(ApiGroup):
@@ -8,3 +9,13 @@ class SdCard(ApiGroup):
 
     def sdcard(self) -> dict:
         return self._connection.get('sdcard/sdcard')
+
+    def sdcardsamba(self) -> dict:
+        return self._connection.get('sdcard/sdcardsamba')
+
+    def printerlist(self) -> dict:
+        return self._connection.get('sdcard/printerlist')
+
+    @authorized_call
+    def share_account(self) -> dict:
+        return self._connection.get('sdcard/share-account')
