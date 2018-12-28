69182577168d9c14aeda07dec4549cf4e077c324
Ville Skyttä <ville.skytta@iki.fi>
2018-12-28 21:35:28 +0200

    Add some more SD card and security methods

--- huawei_lte_api/api/SdCard.py
+++ huawei_lte_api/api/SdCard.py
@@ -7,11 +7,36 @@ class SdCard(ApiGroup):
     def dlna_setting(self) -> dict:
         return self._connection.get('sdcard/dlna-setting')
 
+    @authorized_call
+    def set_dlna_setting(self, enabled: bool, share_all: bool,
+                         share_path: str = '/'):
+        return self._connection.post('sdcard/dlna-setting', {
+            'enabled': int(enabled),
+            'sharepath': share_path,
+            'shareallpath': int(share_all),
+        })
+
     def sdcard(self) -> dict:
         return self._connection.get('sdcard/sdcard')
 
     def sdcardsamba(self) -> dict:
         return self._connection.get('sdcard/sdcardsamba')
+
+    @authorized_call
+    def set_sdcardsamba(self, enabled: bool,
+                        server_name: str = 'homerouter.cpe',
+                        server_description: str = 'samba server',
+                        workgroup_name: str = 'WORKGROUP',
+                        anonymous_access: bool = False,
+                        printer_enabled: bool = True):
+        return self._connection.post('sdcard/sdcardsamba', {
+            'enabled': int(enabled),
+            'servername': server_name,
+            'serverdescription': server_description,
+            'workgroupname': workgroup_name,
+            'anonymousaccess': int(anonymous_access),
+            'printerenable': int(printer_enabled),
+        })
 
     def printerlist(self) -> dict:
         return self._connection.get('sdcard/printerlist')
