005655b9c5ba5853263ae7192db255ce069dff08
Adam Schubert <adam.schubert@sg1-game.net>
2019-03-08 00:11:31 +0100

    New version 1.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='huawei-lte-api',
-    version='1.1.4',
+    version='1.1.5',
     packages=[
         'huawei_lte_api',
         'huawei_lte_api.api',
