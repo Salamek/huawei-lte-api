2e07d93482c0a390669b9294962f72bb7af4220f
Adam Schubert <adam.schubert@sg1-game.net>
2019-06-17 02:45:04 +0200

    chore(doc): Add doc to set_mobile_dataswitch and mobile_dataswitch

--- huawei_lte_api/api/DialUp.py
+++ huawei_lte_api/api/DialUp.py
@@ -4,20 +4,24 @@ from huawei_lte_api.AuthorizedConnection import authorized_call
 
 
 class DialUp(ApiGroup):
-    def mobile_dataswitch(self) ->dict:
+    def mobile_dataswitch(self) -> dict:
+        """
+        Get current LTE modem toggle state
+        :return:
+        """
         return self._connection.get('dialup/mobile-dataswitch')
 
-    def connection(self) ->dict:
+    def connection(self) -> dict:
         return self._connection.get('dialup/connection')
 
-    def dialup_feature_switch(self) ->dict:
+    def dialup_feature_switch(self) -> dict:
         return self._connection.get('dialup/dialup-feature-switch')
 
     @authorized_call
-    def profiles(self) ->dict:
+    def profiles(self) -> dict:
         return self._connection.get('dialup/profiles')
 
-    def auto_apn(self) ->dict:
+    def auto_apn(self) -> dict:
         return self._connection.get('dialup/auto-apn')
 
     def dial(self):
@@ -25,7 +29,12 @@ class DialUp(ApiGroup):
             'Action': 1
         })
 
-    def set_mobile_dataswitch(self, dataswitch: int=0):
+    def set_mobile_dataswitch(self, dataswitch: int = 0) -> dict:
+        """
+        Toggle LTE modem state
+        :param dataswitch: 0 to disable LTE modem, 1 to enable LTE modem
+        :return: dict
+        """
         return self._connection.post('dialup/mobile-dataswitch', {
             'dataswitch': dataswitch
         })
