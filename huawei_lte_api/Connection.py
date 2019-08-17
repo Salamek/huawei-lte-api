934314099f745f5c55fd02b10a972bdff5ab2d3b
Ville Skyttä <ville.skytta@iki.fi>
2019-08-17 10:58:24 +0300

    Support devices that have no CSRF protection

--- huawei_lte_api/Connection.py
+++ huawei_lte_api/Connection.py
@@ -1,3 +1,4 @@
+import logging
 import re
 import urllib.parse
 import dicttoxml
@@ -11,6 +12,8 @@ from huawei_lte_api.exceptions import \
     ResponseErrorSystemBusyException, \
     ResponseErrorLoginCsfrException
 
+
+_LOGGER = logging.getLogger(__name__)
 
 def _try_or_reload_and_retry(fn):
     def wrapped(*args, **kw):
@@ -88,7 +91,9 @@ class Connection:
         if csfr_tokens:
             self.request_verification_tokens = csfr_tokens
         else:
-            self.request_verification_tokens.append(self._get_token())
+            token = self._get_token()
+            if token is not None:
+                self.request_verification_tokens.append(token)
 
     def _build_final_url(self, endpoint: str, prefix: str='api') -> str:
         return urllib.parse.urljoin(self.url + '{}/'.format(prefix), endpoint)
@@ -105,10 +110,11 @@ class Connection:
         headers = {
             'Content-Type': 'application/xml'
         }
-        if len(self.request_verification_tokens) > 1:
-            headers['__RequestVerificationToken'] = self.request_verification_tokens.pop(0)
-        else:
-            headers['__RequestVerificationToken'] = self.request_verification_tokens[0]
+        if self.request_verification_tokens:
+            if len(self.request_verification_tokens) > 1:
+                headers['__RequestVerificationToken'] = self.request_verification_tokens.pop(0)
+            else:
+                headers['__RequestVerificationToken'] = self.request_verification_tokens[0]
 
         response = requests.post(
             self._build_final_url(endpoint, prefix),
@@ -133,7 +139,7 @@ class Connection:
         elif '__RequestVerificationToken' in response.headers:
             self.request_verification_tokens.append(response.headers['__RequestVerificationToken'])
         else:
-            raise ResponseErrorException('Failed to get CSFR from POST response headers', response.status_code)
+            _LOGGER.debug('Failed to get CSFR from POST response headers')
 
         return data
 
@@ -153,5 +159,8 @@ class Connection:
         return self._check_response_status(self._process_response_xml(response.content))
 
     def _get_token(self):
-        data = self.get('webserver/token')
+        try:
+            data = self.get('webserver/token')
+        except ResponseErrorNotSupportedException:
+            return None
         return data['token']
