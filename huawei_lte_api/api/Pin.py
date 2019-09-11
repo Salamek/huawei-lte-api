d58d0cc5f74afa99c5467c265899b51a74ae2c76
Błażej Pawlak <blazej.pawlak2@dk.ibm.com>
2019-09-11 09:47:22 +0200

    feat(pin): adds pin entry function to unlock SIM

--- huawei_lte_api/api/Pin.py
+++ huawei_lte_api/api/Pin.py
@@ -11,3 +11,15 @@ class Pin(ApiGroup):
 
     def save_pin(self) -> dict:
         return self._connection.get('pin/save-pin')
+    
+    def enter_pin(self, pin: int) -> dict:
+        dicttoxml_xargs = {
+            'item_func': lambda x: x[:-1]
+        }
+
+        return self._connection.post('pin/operate', {
+            'OperateType': 0,
+            'CurrentPin': pin,
+            'NewPin': '',
+            'PukCode': ''
+        }, dicttoxml_xargs=dicttoxml_xargs)
