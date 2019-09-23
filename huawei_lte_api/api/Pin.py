9bcf3688c987027ae7c311a07f73f83da3ad9742
Błażej Pawlak <blazej.pawlak2@dk.ibm.com>
2019-09-23 16:56:45 +0200

    fix(pin): adjusts code per code review on PIN operations

--- huawei_lte_api/api/Pin.py
+++ huawei_lte_api/api/Pin.py
@@ -11,15 +11,32 @@ class Pin(ApiGroup):
 
     def save_pin(self) -> dict:
         return self._connection.get('pin/save-pin')
-    
-    def enter_pin(self, pin: int) -> dict:
-        dicttoxml_xargs = {
-            'item_func': lambda x: x[:-1]
-        }
 
-        return self._connection.post('pin/operate', {
-            'OperateType': 0,
-            'CurrentPin': pin,
-            'NewPin': '',
-            'PukCode': ''
-        }, dicttoxml_xargs=dicttoxml_xargs)
+    def operate(self, operate_type: int = 0, current_pin: int = None, new_pin: int = None, puk_code: int = None) -> dict:
+    """
+    Parameters
+    ----------
+    operate_type : int
+        Operation type to perform (default is `0`).
+        0 - verify PIN
+        1 - enable PIN verification
+        2 - disable PIN verification
+        3 - set new PIN
+        4 - use of the PUK code
+    current_pin : int
+        Current PIN number (default is `None`).
+    new_pin : int
+        New PIN number to set (default is `None`).
+    puk_code : int
+        PUK code to use in case it is required by the device (default is `None`).
+    """
+    dicttoxml_xargs = {
+        'item_func': lambda x: x[:-1]
+    }
+
+    return self._connection.post('pin/operate', {
+        'OperateType': operate_type,
+        'CurrentPin': current_pin,
+        'NewPin': new_pin,
+        'PukCode': puk_code
+    }, dicttoxml_xargs=dicttoxml_xargs)
