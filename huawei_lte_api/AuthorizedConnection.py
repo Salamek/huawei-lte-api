Salamek/huawei-lte-api

commit ed1011c8d9df1dfb02a4937e396036f245a37c31

Fix issue where login info passed inside url will throw Session error on login, may solve #51

huawei_lte_api/AuthorizedConnection.py
@@ -11,13 +11,18 @@ class AuthorizedConnection(Connection):
 
     def __init__(self, url: str, username: Optional[str]=None, password: Optional[str]=None,
                  login_on_demand: bool=False, timeout: Union[float, Tuple[float, float], None] = None):
-        super(AuthorizedConnection, self).__init__(url, timeout=timeout)
         parsed_url = urlparse(url)
-
+        clear_url = '{scheme}://{hostname}{path}'.format(
+            scheme=parsed_url.scheme,
+            hostname=parsed_url.hostname,
+            path=parsed_url.path
+        )
+        super(AuthorizedConnection, self).__init__(clear_url, timeout=timeout)
         username = username if username else parsed_url.username
         password = password if password else parsed_url.password
 
         from huawei_lte_api.api.User import User  # pylint: disable=cyclic-import
+
         self.user = User(self, username, password)
 
         if not login_on_demand:

commit d5356aa0087be53adf7062b6a615354cce17cab8

Don't drop port (or anything else but auth info) from AuthorizedConnection URL
Broken in ed1011c8d9df1dfb02a4937e396036f245a37c31

Refs https://github.com/home-assistant/core/issues/35583

huawei_lte_api/AuthorizedConnection.py
@@ -1,6 +1,6 @@
 import datetime
 from typing import Optional, Tuple, Union
-from urllib.parse import urlparse
+from urllib.parse import urlparse, urlunparse
 from huawei_lte_api.Connection import Connection
 
 
@@ -11,12 +11,13 @@ class AuthorizedConnection(Connection):
 
     def __init__(self, url: str, username: Optional[str]=None, password: Optional[str]=None,
                  login_on_demand: bool=False, timeout: Union[float, Tuple[float, float], None] = None):
+        # Auth info embedded in the URL may reportedly cause problems, strip it
         parsed_url = urlparse(url)
-        clear_url = '{scheme}://{hostname}{path}'.format(
-            scheme=parsed_url.scheme,
-            hostname=parsed_url.hostname,
-            path=parsed_url.path
-        )
+        clear_url = urlunparse((
+            parsed_url.scheme,
+            parsed_url.netloc.rpartition("@")[-1],
+            *parsed_url[2:]
+        ))
         super(AuthorizedConnection, self).__init__(clear_url, timeout=timeout)
         username = username if username else parsed_url.username
         password = password if password else parsed_url.password