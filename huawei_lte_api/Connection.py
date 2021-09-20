a71517b53d0865e31d7bda0c2349c6ec31454acb
Adam Schubert <adam.schubert@sg1-game.net>
2021-09-20 13:29:01 +0200

    fix(Connection): Pass requests_session to Session from Connection

--- huawei_lte_api/Connection.py
+++ huawei_lte_api/Connection.py
@@ -1,4 +1,5 @@
 import warnings
+import requests
 from typing import Optional, Tuple, Union
 from huawei_lte_api.Session import Session
 from huawei_lte_api.api.User import UserSession
@@ -11,7 +12,8 @@ class Connection(Session):
                  username: Optional[str] = None,
                  password: Optional[str] = None,
                  login_on_demand: bool = False,
-                 timeout: Union[float, Tuple[float, float], None] = None
+                 timeout: Union[float, Tuple[float, float], None] = None,
+                 requests_session: requests.Session = None
                  ):
         parsed_url = urlparse(url)
 
@@ -19,8 +21,7 @@ class Connection(Session):
         username = username if username else parsed_url.username
         password = password if password else parsed_url.password
 
-        #from huawei_lte_api.api.User import UserSession  # pylint: disable=cyclic-import,import-outside-toplevel
-        super().__init__(url, timeout=timeout)
+        super().__init__(url, timeout=timeout, requests_session=requests_session)
         self.user_session = UserSession(self, username, password) if username else None
 
         if login_on_demand:
