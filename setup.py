412672861c541df5d563a2a6293e473d68c481d0
Adam Schubert <adam.schubert@sg1-game.net>
2021-09-20 18:12:33 +0200

    feat(Tests): Get tests working correctly

--- setup.py
+++ setup.py
@@ -1,6 +1,11 @@
 from setuptools import setup, find_packages
 
 
+def read_readme() -> str:
+    with open('README.md', 'r', encoding='utf-8') as f:
+        return f.read()
+
+
 setup(
     name='huawei-lte-api',
     version='1.4.18',
@@ -8,15 +13,17 @@ setup(
     package_data={'huawei_lte_api': ['py.typed']},
     install_requires=[
         'requests',
-        'dicttoxml',
         'xmltodict',
+    ],
+    tests_require=[
+        'tox'
     ],
     url='https://github.com/Salamek/huawei-lte-api',
     license='LGPL-3.0 ',
     author='Adam Schubert',
     author_email='adam.schubert@sg1-game.net',
     description='API For huawei LAN/WAN LTE Modems',
-    long_description=open('README.md', encoding='utf-8').read(),
+    long_description=read_readme(),
     long_description_content_type='text/markdown',
     test_suite='tests',
     classifiers=[
