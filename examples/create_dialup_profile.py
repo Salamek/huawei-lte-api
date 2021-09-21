eb95c7757aab2a1c71ab13a480b840ac3cd7e73f
Adam Schubert <adam.schubert@sg1-game.net>
2021-09-21 15:13:27 +0200

    feat(Examples): Added example on how to add new dialup profile

---
+++ examples/create_dialup_profile.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+Example code on how to create dialup profile, you can try it by running:
+python3 connect_with_proxy.py http://admin:PASSWORD@192.168.8.1/ name
+"""
+from argparse import ArgumentParser
+
+import requests
+
+from huawei_lte_api.Client import Client
+from huawei_lte_api.Connection import Connection
+
+parser = ArgumentParser()
+parser.add_argument('url', type=str)
+parser.add_argument('name', type=str)
+parser.add_argument('--username', type=str)
+parser.add_argument('--password', type=str)
+args = parser.parse_args()
+
+with Connection(
+        args.url,
+        username=args.username,
+        password=args.password,
+) as connection:
+    client = Client(connection)
+    print(client.dial_up.create_profile(args.name))
