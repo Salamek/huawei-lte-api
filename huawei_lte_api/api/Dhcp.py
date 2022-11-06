Salamek/huawei-lte-api

commit 2facc95324f3f0a40c4d4143086da49987c698ed

set dhcp and connection settings

huawei_lte_api/api/Dhcp.py
@@ -1,5 +1,6 @@
+from typing import Optional
 from huawei_lte_api.ApiGroup import ApiGroup
-from huawei_lte_api.Session import GetResponseType
+from huawei_lte_api.Session import GetResponseType, SetResponseType
 
 
 class Dhcp(ApiGroup):
@@ -22,3 +23,52 @@ class Dhcp(ApiGroup):
         :return:
         """
         return self._session.get('dhcp/static-addr-info')
+
+    def set_settings(
+        self,
+        dhcp_ip_address: str = "192.168.0.1",
+        dhcp_lan_netmask: str = "255.255.255.0",
+        dhcp_status: bool = True,
+        dhcp_start_ip_range: int = 100,
+        dhcp_end_ip_range: int = 200,
+        dhcp_lease_time: int = 86400,
+        dns_status: bool = True,
+        primary_dns: Optional[str] = None,
+        secondary_dns: Optional[str] = None,
+        show_dns_setting: bool = True,
+    ) -> SetResponseType:
+        """
+        Endoint found in a custom firmware to configure DHCP server settings.
+        :param dhcp_ip_address: IP address of dhcp server.
+        :param dhcp_lan_netmask: Netmask for dhcp server.
+        :param dhcp_status: Turn dhcp server on/off.
+        :param dhcp_start_ip_range: Lease ip range from.
+        :param dhcp_end_ip_range: Lease ip range till.
+        :param dhcp_lease_time: IP lease duration.
+        :param dns_status: Unknown usage.
+        :param primary_dns: Primary DNS server IP.
+        :param secondary_dns: Secondary DNS server IP.
+        :param show_dns_setting: Unknown usage.
+        :return SetResponseType: Set response type.
+        """
+        
+        ip_address_parts = dhcp_ip_address.split(".")
+        ip_address_parts.pop(-1)
+        dhcp_start_ip_address = ".".join(ip_address_parts) + "." + str(dhcp_start_ip_range)
+        dhcp_end_ip_address = ".".join(ip_address_parts) + "." + str(dhcp_end_ip_range)
+
+        return self._session.post_set(
+            'dhcp/settings',
+            {
+                'DhcpIPAddress': dhcp_ip_address,
+                'DhcpLanNetmask': dhcp_lan_netmask,
+                'DhcpStatus': 1 if dhcp_status else 0,
+                'DhcpStartIPAddress': dhcp_start_ip_address,
+                'DhcpEndIPAddress': dhcp_end_ip_address,
+                'DhcpLeaseTime': dhcp_lease_time,
+                'DnsStatus': 1 if dns_status else 0,
+                'PrimaryDns': primary_dns,
+                'SecondaryDns': secondary_dns,
+                'ShowDnsSetting': 1 if show_dns_setting else 0,
+            }
+        )

huawei_lte_api/api/DialUp.py
@@ -85,3 +85,35 @@ class DialUp(ApiGroup):
                 'iptype': ip_type.value
             }
         }, is_encrypted=True)
+
+    def set_connection_settings(
+        self,
+        roam_auto_connect_enable: bool = True,
+        max_idle_time: int = 0,
+        connect_mode: int = 0,
+        mtu: int = 1500,
+        auto_dial_switch: bool = True,
+        pdp_always_on: bool = True,
+    ) -> SetResponseType:
+        """
+        Set connection settings.
+        :param roam_auto_connect_enable: Enable/Disable data roaming.
+        :param max_idle_time: Auto disconnect interval, 0 is always on.
+        :param connect_mode: Unknown usage.
+        :param mtu: Unknown usage.
+        :param auto_dial_switch: Unknown usage.
+        :param pdp_always_on: Unknown usage.
+        :return SetResponseType: Set response type.
+        """
+
+        return self._session.post_set(
+            'dialup/connection',
+            {
+                'RoamAutoConnectEnable': 1 if roam_auto_connect_enable else 0,
+                'MaxIdelTime': max_idle_time,
+                'ConnectMode': connect_mode,
+                'MTU': mtu,
+                'auto_dial_switch': 1 if auto_dial_switch else 0,
+                'pdp_always_on': 1 if pdp_always_on else 0,
+            }
+        )

commit b2b9ce584599f5105f50e0a2ebf737bfb4bbf3da

Pylint fixes
Regression from 2facc95324f3f0a40c4d4143086da49987c698ed.

huawei_lte_api/api/Dhcp.py
@@ -24,7 +24,7 @@ class Dhcp(ApiGroup):
         """
         return self._session.get('dhcp/static-addr-info')
 
-    def set_settings(
+    def set_settings(  # pylint: disable=too-many-arguments
         self,
         dhcp_ip_address: str = "192.168.0.1",
         dhcp_lan_netmask: str = "255.255.255.0",
@@ -51,7 +51,7 @@ class Dhcp(ApiGroup):
         :param show_dns_setting: Unknown usage.
         :return SetResponseType: Set response type.
         """
-        
+
         ip_address_parts = dhcp_ip_address.split(".")
         ip_address_parts.pop(-1)
         dhcp_start_ip_address = ".".join(ip_address_parts) + "." + str(dhcp_start_ip_range)