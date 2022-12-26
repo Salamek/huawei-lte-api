aa74bd7eca9ebfe6fcecfea9a6f6f84a82821973
Adam Schubert <adam.schubert@sg1-game.net>
2022-12-26 21:39:32 +0100

    chore(enums.device): Extends ControlModeEnum of BACKUP_CONFIGURATION

--- huawei_lte_api/enums/device.py
+++ huawei_lte_api/enums/device.py
@@ -13,6 +13,10 @@ class AntennaTypeEnum(enum.IntEnum):
 class ControlModeEnum(enum.IntEnum):
     REBOOT = 1
     RESET = 2  # Resets device into factory settings
+
+    # Backups configuration, it can be downloaded later from http://192.168.8.1/nvram.bak
+    # it is Base64 encoded nvram dump, you need to be authorized to download this file
+    BACKUP_CONFIGURATION = 3
     POWER_OFF = 4
 
 
