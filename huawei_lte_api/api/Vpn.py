3b5ca4f862b95abb73562a68858a072b9aacca9b
Mehdi Rahimi <mehdii.rh78@gmail.com>
2023-06-12 18:03:51 +0330

    update: add response type

--- huawei_lte_api/api/Vpn.py
+++ huawei_lte_api/api/Vpn.py
@@ -19,7 +19,7 @@ class Vpn(ApiGroup):
     def pptp_settings(self) -> GetResponseType:
         return self._session.get('vpn/pptp_settings')
 
-    def toggle_status(self, enable=True):
+    def toggle_status(self, enable=True) -> GetResponseType:
         data = {
             "enable": "1" if enable else "0",
         }
