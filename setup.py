0dcbb45e65e681973445cafce564fd7450833fd5
Adam Schubert <adam.schubert@sg1-game.net>
2024-01-07 15:09:34 +0100

    New version 1.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read_readme() -> str:
 
 setup(
     name='huawei-lte-api',
-    version='1.7.3',
+    version='1.8.0',
     packages=find_packages(exclude=['tests', 'tests.*']),
     package_data={'huawei_lte_api': ['py.typed']},
     install_requires=[
